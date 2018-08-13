1860b37d8eb559022b92f9a07f2944a6fd00b052
Alejandro Endo <alejoendo@hotmail.com>
2018-08-12 19:11:32 -0500

    Commit inicial

---
+++ PDG2_VR/Assets/Scripts/Photon/PhotonNetworkManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonNetworkManager : MonoBehaviour {
+
+    [SerializeField] private Text connectText;
+    [SerializeField] private GameObject lobbyCamera;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private GameObject player;
+
+    private void Start () {
+        PhotonNetwork.ConnectUsingSettings("0.2.0");
+	}
+	
+    public virtual void OnJoinedLobby() {
+        Debug.Log("Joined to loby");
+        PhotonNetwork.JoinOrCreateRoom("InnLab", null, null);
+    }
+
+    public virtual void OnJoinedRoom() {
+        Debug.Log("On da jaus");
+        PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity, 0);
+        lobbyCamera.SetActive(false);
+    }
+
+
+	private void Update () {
+        connectText.text = PhotonNetwork.connectionStateDetailed.ToString();
+	}
+}
---
+++ PDG2_VR/Assets/Scripts/Photon/PlayerNetwork.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNetwork : MonoBehaviour {
+
+    [SerializeField] private GameObject playerCamera;
+    [SerializeField] private MonoBehaviour[] playerControlScript;
+
+    private PhotonView photonView;
+
+    private void Start() {
+        photonView = GetComponent<PhotonView>();
+
+        Initialize();
+    }
+
+    private void Initialize() {
+        if (!photonView.isMine) {
+            playerCamera.SetActive(false);
+
+            foreach (MonoBehaviour m in playerControlScript) {
+                m.enabled = false;
+            }
+        }
+    }
+
+}
