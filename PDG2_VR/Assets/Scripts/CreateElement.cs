5cb6cd77bf64dd5c5e0df75579c37878a3a6bfab
Alejandro Endo <alejoendo@hotmail.com>
2018-08-20 09:05:15 -0500

    Re-estructuracion proyecto
    Correccion de algunos assets y funcionamiento de watson, además de la arquitectura basica para los scriptable objects

---
+++ PDG2_VR/Assets/Scripts/CreateElement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateElement : MonoBehaviour {
+
+    [SerializeField] private Camera cam;
+    [SerializeField] private GameObject box;
+
+    private Ray ray;
+    private RaycastHit hit;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit)) {
+                PhotonNetwork.Instantiate(box.name, hit.transform.position + hit.normal, Quaternion.identity, 0);
+                Debug.Log("aaa");
+            }
+        }
+    }
+}
