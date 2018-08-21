0717fb84e1f051ef9ea1232403be188245676335
Alejandro Endo <alejoendo@hotmail.com>
2018-08-21 07:46:30 -0500

    avance

---
+++ PDG2_VR/Assets/Scripts/Scriptable/ResponsiveManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResponsiveManager : MonoBehaviour {
+
+    [Header("Text Configuration")]
+    [SerializeField]private RectTransform textTransform;
+
+    private float initialWidth;
+    private float initialHeight;
+
+    private float width;
+    private float height;
+
+    [Header("Object configuration")]
+    [SerializeField] private Transform go;
+
+	void Start () {
+        initialWidth = textTransform.sizeDelta.x;
+        initialHeight = textTransform.sizeDelta.y;
+	}
+	
+	void Update () {
+        textTransform.localScale = new Vector3(1 / transform.localScale.x, 1 / transform.localScale.y, 1 / transform.localScale.z);
+
+        width = initialWidth * transform.localScale.x;
+        height = initialHeight * transform.localScale.y;
+
+        textTransform.sizeDelta = new Vector2(width, height);
+	}
+}
---
+++ PDG2_VR/Assets/Scripts/Scriptable/ResponsiveObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResponsiveObject : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other) {
+        //if (other.CompareTag("Boundarie")) {
+            //transform.localScale = new Vector3(transform.localScale.x - 0.01f, transform.localScale.y - 0.01f, transform.localScale.z - 0.01f);
+        //Debug.Log("DUDUDUDU");
+        //}
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.CompareTag("Boundarie")) {
+            transform.localScale = new Vector3(transform.localScale.x - 0.01f, transform.localScale.y - 0.01f, transform.localScale.z - 0.01f);
+        //Debug.Log("DUDUDUDU");
+        }
+    }
+
+}
