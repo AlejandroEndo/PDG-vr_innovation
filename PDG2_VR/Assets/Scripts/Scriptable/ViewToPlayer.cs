e8abd950b5a656575ab283b38d1eeb7760499587
Alejandro Endo <alejoendo@hotmail.com>
2018-08-29 19:04:54 -0500

    Working on feedback and bubbles

---
+++ PDG2_VR/Assets/Scripts/Scriptable/ViewToPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewToPlayer : MonoBehaviour {
+
+    [SerializeField] private Transform player;
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+        this.transform.LookAt(player.position);
+	}
+}
