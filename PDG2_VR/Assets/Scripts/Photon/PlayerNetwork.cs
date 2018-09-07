e1e967fdcbb5836c1b71d211a3744de315df6f1b
Alejandro Endo <alejoendo@hotmail.com>
2018-09-07 18:54:54 -0500

    Ajuste grafico

--- PDG2_VR/Assets/Scripts/Photon/PlayerNetwork.cs
+++ PDG2_VR/Assets/Scripts/Photon/PlayerNetwork.cs
@@ -11,7 +11,11 @@ public class PlayerNetwork : MonoBehaviour {
     [SerializeField] private Transform left;
     [SerializeField] private Transform right;
 
-    [SerializeField] private GameObject body;
+    private GameObject body;
+
+    private Transform headVR;
+    private Transform leftVR;
+    private Transform rightVR;
 
     private PhotonView photonView;
 
@@ -23,23 +27,34 @@ public class PlayerNetwork : MonoBehaviour {
         Initialize();
     }
 
+    //VERSION PARA PC
     private void Initialize() {
         if (!photonView.isMine) {
             for (int i = 0; i < playerCamera.Length; i++) {
-                playerCamera[i].SetActive(false);
+                //playerCamera[i].SetActive(false);
             }
 
             foreach (MonoBehaviour m in playerControlScript) {
-                m.enabled = false;
+                //m.enabled = false;
             }
         } else {
-            left.SetParent(body.transform.GetChild(0).transform);
+            /*left.SetParent(body.transform.GetChild(0).transform);
             right.SetParent(body.transform.GetChild(1).transform);
-            head.SetParent(body.transform.GetChild(2).transform);
+            head.SetParent(body.transform.GetChild(2).transform);*/
+
+            leftVR = body.transform.GetChild(0).transform;
+            rightVR = body.transform.GetChild(1).transform;
+            headVR = body.transform.GetChild(2).transform;
 
             head.localPosition = Vector3.zero;
             left.localPosition = Vector3.zero;
             right.localPosition = Vector3.zero;
         }
+    }
+
+    private void Update() {
+        head.position = headVR.position;
+        left.position = leftVR.position;
+        right.position = rightVR.position;
     }
 }
