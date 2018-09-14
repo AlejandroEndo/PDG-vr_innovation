5e0ef9f1d6498489938a3d0edf805db0a53a783f
Alejandro Endo <alejoendo@hotmail.com>
2018-09-13 19:02:49 -0500

    adaptacion de cuadrados para rotacion y cambio de seguimiento
    Ahora los nuevos post-it se pueden ajustar para que muestren la informacion que el usuario desea ver, esto puede darse desde indibidualmente hasta en general

--- PDG2_VR/Assets/Scripts/Scriptable/ViewToPlayer.cs
+++ PDG2_VR/Assets/Scripts/Scriptable/ViewToPlayer.cs
@@ -6,13 +6,29 @@ public class ViewToPlayer : MonoBehaviour {
 
     [SerializeField] private Transform mCamera;
 
-    void Start() {
+    [SerializeField] private Vector3[] boxSide;
+    [SerializeField] [Range(0, 3)] private int index;
+    [SerializeField] [Range(0f, 10f)] private float rotationSpeed;
 
+    void Start() {
+        boxSide = new Vector3[4];
+        boxSide[0] = Vector3.forward;
+        boxSide[1] = Vector3.right;
+        boxSide[2] = Vector3.back;
+        boxSide[3] = Vector3.left;
     }
 
 
     void Update() {
-        transform.LookAt(transform.position + mCamera.transform.rotation * Vector3.back,
-           mCamera.transform.rotation * Vector3.up);
+        //transform.LookAt(transform.position + mCamera.transform.rotation * boxSide[index], mCamera.transform.rotation * boxSide[index]);
+        if (transform.parent == null) {
+            //transform.LookAt(transform.position + mCamera.transform.rotation * boxSide[index], mCamera.transform.rotation * Vector3.up);
+
+            Quaternion OriginalRot = transform.rotation;
+            transform.LookAt(transform.position + mCamera.transform.rotation * boxSide[index], mCamera.transform.rotation * Vector3.up);
+            Quaternion NewRot = transform.rotation;
+            transform.rotation = OriginalRot;
+            transform.rotation = Quaternion.Lerp(transform.rotation, NewRot, rotationSpeed * Time.deltaTime);
+        }
     }
 }
