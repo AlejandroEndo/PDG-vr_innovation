86f2964aae81d55256fb3af459708ff6e08a3c42
Alejandro Endo <alejoendo@hotmail.com>
2018-09-21 15:56:08 -0500

    Working on tools

---
+++ PDG2_VR/Assets/Scripts/Tools/ToolofTools.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolofTools : MonoBehaviour {
+
+    [SerializeField] private Transform VRcamera;
+
+    private Vector3 pos;
+
+	void Start () {
+        pos = transform.localPosition;
+	}
+	
+	void Update () {
+        transform.position = new Vector3(VRcamera.position.x + pos.x, VRcamera.position.y + pos.y, VRcamera.position.z + pos.z);
+        transform.rotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, VRcamera.transform.localRotation.eulerAngles.y, transform.localRotation.eulerAngles.z);
+	}
+}
