936a8a8a9b385af0f955aada19654f05fae8e61b
Alejandro Endo <alejoendo@hotmail.com>
2018-10-06 17:11:33 -0500

    Grab input changed

--- PDG2_VR/Assets/Scripts/Scriptable/Writter.cs
+++ PDG2_VR/Assets/Scripts/Scriptable/Writter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
 
+
 public class Writter : VRTK_InteractableObject {
 
     
@@ -10,14 +11,23 @@ public class Writter : VRTK_InteractableObject {
     private RaycastHit touch;
     private Quaternion lastAngle;
     private bool lastTouch;
+    public GameObject floor;
+    
+
 
     // Use this for initialization
     void Start() {
      
         this.whiteboard = GameObject.Find("Whiteboard").GetComponent<Whiteboard>();
+
+       //GetComponent<VRTK_ControllerEvents>().TriggerPressed += new ControllerInteractionEventHandler(DoTriggerPressed);
     }
 
-    
+    /*private void DoTriggerPressed(object sender, ControllerInteractionEventArgs e) {
+
+        floor.SetActive(false);
+    }*/
+
     void Update() {
         float tipHeight = transform.Find("Tip").transform.localScale.y;
         Vector3 tip = transform.Find("Tip/TouchPoint").transform.position;
@@ -28,7 +38,9 @@ public class Writter : VRTK_InteractableObject {
             tipHeight *= 1.1f;
         }
 
-      
+           
+        
+
         if (Physics.Raycast(tip, transform.up, out touch, tipHeight)) {
             if (!(touch.collider.tag == "Whiteboard")) return;
             this.whiteboard = touch.collider.GetComponent<Whiteboard>();
@@ -55,4 +67,6 @@ public class Writter : VRTK_InteractableObject {
             transform.rotation = lastAngle;
         }
     }
+
+  
 }
