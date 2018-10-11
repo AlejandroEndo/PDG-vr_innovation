0f11ac753d19b37c7bda4064fabb7b323b09ee77
Alejandro Endo <alejoendo@hotmail.com>
2018-10-11 14:33:33 -0500

    menú radial implementado

---
+++ PDG2_VR/Assets/Scripts/RadialMenuCreator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class RadialMenuCreator : MonoBehaviour {
+
+
+    [Header("Right Hand")]
+    [SerializeField] private GameObject marker;
+    [SerializeField] private VRTK_ControllerTooltips rightTextField;
+
+    [Header("Left Hand")]
+    [SerializeField] private GameObject leftTextField;
+
+    void Start() {
+
+    }
+
+    #region RIGHT CONTROLLER
+    public void CreateMarker() {
+        GameObject m = Instantiate(marker, transform.position, Quaternion.identity);
+    }
+
+    public void ChangeTextField(string t) {
+        rightTextField.touchpadText = t;
+    }
+
+    #endregion
+
+    #region LEFT CONTROLLER
+    public void a() {
+
+    }
+
+    #endregion
+
+}
