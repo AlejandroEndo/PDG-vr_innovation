6adc71e1127e29014fe591ca0c79f069ec97bce0
Alejandro Endo <alejoendo@hotmail.com>
2018-10-11 19:31:56 -0500

    Menú radial al 100%

--- PDG2_VR/Assets/Scripts/RadialMenuCreator.cs
+++ PDG2_VR/Assets/Scripts/RadialMenuCreator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
@@ -9,9 +10,15 @@ public class RadialMenuCreator : MonoBehaviour {
     [Header("Right Hand")]
     [SerializeField] private GameObject marker;
     [SerializeField] private VRTK_ControllerTooltips rightTextField;
+    [SerializeField] private Transform RightHand;
 
     [Header("Left Hand")]
     [SerializeField] private GameObject leftTextField;
+    [SerializeField] private GameObject cubeYellow;
+    [SerializeField] private GameObject cubePink;
+    [SerializeField] private GameObject cubeGreen;
+    [SerializeField] private GameObject cubeBlue;
+    [SerializeField] private Transform LeftHand;
 
     void Start() {
 
@@ -19,7 +26,13 @@ public class RadialMenuCreator : MonoBehaviour {
 
     #region RIGHT CONTROLLER
     public void CreateMarker() {
-        GameObject m = Instantiate(marker, transform.position, Quaternion.identity);
+        try {
+            GameObject m = GameObject.FindGameObjectWithTag("Marker");
+            m.transform.position = RightHand.position;
+        } catch (Exception e) {
+            Debug.Log("[CREANDO MARCADOR]");
+            GameObject m = Instantiate(marker, RightHand.position, Quaternion.identity);
+        }
     }
 
     public void ChangeTextField(string t) {
@@ -29,10 +42,21 @@ public class RadialMenuCreator : MonoBehaviour {
     #endregion
 
     #region LEFT CONTROLLER
-    public void a() {
-
+    public void CreatePostit_Yellow() {
+        GameObject c = Instantiate(cubeYellow, LeftHand.position, Quaternion.identity);
     }
 
+    public void CreatePostit_Pink() {
+        GameObject c = Instantiate(cubePink, LeftHand.position, Quaternion.identity);
+    }
+
+    public void CreatePostit_Green() {
+        GameObject c = Instantiate(cubeGreen, LeftHand.position, Quaternion.identity);
+    }
+
+    public void CreatePostit_Blue() {
+        GameObject c = Instantiate(cubeBlue, LeftHand.position, Quaternion.identity);
+    }
     #endregion
 
 }
